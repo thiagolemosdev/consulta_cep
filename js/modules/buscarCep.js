f49d50b9344fe3bd7467f37df843838f232d8922
Thiago Lemos <lemoos.th@gmail.com>
2022-02-16 00:47:58 -0300

    Concluido

--- js/modules/buscarCep.js
+++ js/modules/buscarCep.js
@@ -1,42 +1,42 @@
-const btnBuscar = document.querySelector("[data-buscar]");
-const resposta = document.querySelector("[data-resposta]");
+export default function initBuscar() {
+  const btnBuscar = document.querySelector("[data-buscar]");
+  const resposta = document.querySelector("[data-resposta]");
 
-btnBuscar.addEventListener("click", buscar);
+  btnBuscar.addEventListener("click", buscar);
 
-function buscar(el) {
-  el.preventDefault();
-  resposta.innerHTML = `<div> </div>`;
-  const cep = document.querySelector("[data-cep]").value;
-  fetch(`https://viacep.com.br/ws/${cep}/json/`)
-    .then((r) => {
-      return r.json();
-    })
-    .then((cep) => {
-      html(cep);
-    });
+  function buscar(el) {
+    el.preventDefault();
+    resposta.innerHTML = `<div> </div>`;
+    const cep = document.querySelector("[data-cep]").value;
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then((r) => {
+        return r.json();
+      })
+      .then((cep) => {
+        html(cep);
+      });
 
-  function html(cep) {
-    const rua = document.createElement("li");
-    rua.innerText = `Logradouro: ${cep.logradouro}`;
+    function html(cep) {
+      const rua = document.createElement("li");
+      rua.innerText = `Logradouro: ${cep.logradouro}`;
 
-    const bairro = document.createElement("li");
-    bairro.innerText = `Bairro: ${cep.bairro}`;
+      const bairro = document.createElement("li");
+      bairro.innerText = `Bairro: ${cep.bairro}`;
 
-    const cidade = document.createElement("li");
-    cidade.innerText = `Cidade: ${cep.localidade}`;
+      const cidade = document.createElement("li");
+      cidade.innerText = `Cidade: ${cep.localidade}`;
 
-    const uf = document.createElement("li");
-    uf.innerText = `Estado: ${cep.uf}`;
+      const uf = document.createElement("li");
+      uf.innerText = `Estado: ${cep.uf}`;
 
-    const resultado = document.createElement("ul");
-    resultado.appendChild(rua);
-    resultado.appendChild(bairro);
-    resultado.appendChild(cidade);
-    resultado.appendChild(uf);
+      const resultado = document.createElement("ul");
+      resultado.appendChild(rua);
+      resultado.appendChild(bairro);
+      resultado.appendChild(cidade);
+      resultado.appendChild(uf);
 
-    resposta.appendChild(resultado);
-    resposta.classList.add("ativo");
+      resposta.appendChild(resultado);
+      resposta.classList.add("ativo");
+    }
   }
 }
-
-export default function initBuscar() {}
